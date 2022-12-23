36a898c7416943ba4fc3c456f9b5cc1c841d3430
KumarAmitt <amitt2015@outlook.com>
2022-12-23 16:48:04 +0530

    Add secure password

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
   before_save { self.email = email.downcase }
 
+  has_secure_password
+
   VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false },
