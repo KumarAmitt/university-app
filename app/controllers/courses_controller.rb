d8fcec6856f2a6f24152e5fae52fd14e3d7f6c38
KumarAmitt <amitt2015@outlook.com>
2022-12-24 10:21:58 +0530

    Implement authorization

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,6 @@
 class CoursesController < ApplicationController
+
+  skip_before_action :require_user, only: %i[index]
   def index
     @courses = Course.all
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,24 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_user, only: %i[new create]
   def new
   end
 
   def create
+    student = Student.find_by(email: params[:sessions][:email].downcase)
+    if student && student.authenticate(params[:sessions][:password])
+      session[:student_id] = student.id
+      flash[:notice] = 'You successfully logged in'
+      redirect_to student
+    else
+      flash.now[:notice] = 'Something went wrong. Please try again.'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = 'You have successfully logged out'
+    redirect_to login_path
   end
 
 end
